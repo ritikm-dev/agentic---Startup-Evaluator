ba816e6e5c9ef36d1e594c54b7fa66e20f2f91b5
subasanthosh <subasanthosh2007@gmail.com>
2026-02-25 16:40:05 +0530

    frontend

--- frontend/entrepreneur_web/src/App.jsx
+++ frontend/entrepreneur_web/src/App.jsx
@@ -2,8 +2,69 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import { useNavigate } from 'react-router-dom'
+
 function App() {
+
   const Navigate = useNavigate();
+  async function handle_single() {
+
+    try {
+      const res = await fetch("http://localhost:8000/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "message": "single"
+        })
+      })
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        Navigate("/single")
+      }
+      else {
+        alert("Something went wrong")
+        console.log("hi");
+      }
+
+    }
+    catch (error) {
+      alert("Server not responding please try again later or check your connection");
+      console.log(error);
+    }
+  }
+
+async function handle_team() {
+
+    try {
+      const res = await fetch("http://localhost:8000/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          "message": "Team"
+        })
+      })
+      const data = await res.json();
+      console.log(data);
+      if (res.ok) {
+        Navigate("/team")
+      }
+      else {
+        alert("Something went wrong")
+        console.log("hi");
+      }
+
+    }
+    catch (error) {
+      alert("Server not responding please try again later or check your connection");
+      console.log(error);
+    }
+  }
+
+
   return (
     <>
       <title>VENTURE STUDIO</title>
@@ -12,8 +73,8 @@ function App() {
       </div>
       <h1 className='page-title'>READY WITH YOUR IDEA ?</h1>
       <div className='button-container'>
-        <button onClick={()=>{Navigate("/single")}}>SINGLE FOUNDER</button> 
-        <button onClick={()=>{Navigate("/team")}}>TEAM</button>
+        <button onClick={handle_single}>SINGLE FOUNDER</button>
+        <button onClick={handle_team}>TEAM</button>
       </div>
     </>
   )
